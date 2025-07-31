c0eab55a99997a1be160d3d2924536a8911a17bb
Alberes <alberes.menezes@gmail.com>
2025-07-31 17:18:32 -0300

    Not null validation in request

--- src/main/java/io/github/alberes/wlperfectnumber/controllers/dto/PerfectNumberRequestDto.java
+++ src/main/java/io/github/alberes/wlperfectnumber/controllers/dto/PerfectNumberRequestDto.java
@@ -1,12 +1,15 @@
 package io.github.alberes.wlperfectnumber.controllers.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Range;
 
 @Schema(name = "PerfectNumberRequest")
 public record PerfectNumberRequestDto(
+        @NotNull(message = "Fill this field")
         @Range(min = 1, max = Integer.MAX_VALUE, message = "Fill in this field with a value between 1 and " + Integer.MAX_VALUE)
         Integer start,
+        @NotNull(message = "Fill this field")
         @Range(min = 1, max = Integer.MAX_VALUE, message = "Fill in this field with a value between 1 and " + Integer.MAX_VALUE)
         Integer end) {
 }
